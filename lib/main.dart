c625264ef100dc1bd11e1232f33a8f1153ae869a
Dasha <dashabella1@gmail.com>
2024-02-24 19:52:43 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
-import 'package:rt/pages/home.dart';
+import 'package:rt/firebase_options.dart';
+import 'views/home.dart';
+import 'package:flutter/services.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // строка позволяет запустить firebase до запуска приложения
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MaterialApp(
-    home: Home(),
-  ));
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  runApp(const ToDoList());
+}
+
+class ToDoList extends StatelessWidget {
+  const ToDoList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
 }
---
+++ lib/widgets/delete_task_dialog.dart
@@ -0,0 +1,73 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class DeleteTaskDialog extends StatefulWidget {
+  final String taskId, taskName;
+
+  const DeleteTaskDialog(
+      {Key? key, required this.taskId, required this.taskName})
+      : super(key: key);
+
+  @override
+  State<DeleteTaskDialog> createState() => _DeleteTaskDialogState();
+}
+
+class _DeleteTaskDialogState extends State<DeleteTaskDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      scrollable: true,
+      title: const Text(
+        'Удалить запись?',
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 16, color: Colors.brown),
+      ),
+      content: SizedBox(
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              const Text(
+                'Вы уверены, что хотите удалить данную запись?',
+                style: TextStyle(fontSize: 14),
+              ),
+              const SizedBox(height: 15),
+              Text(
+                widget.taskName.toString(),
+                style:
+                    const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 15),
+            ],
+          ),
+        ),
+      ),
+      actions: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context, rootNavigator: true).pop();
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.grey,
+          ),
+          child: const Text('Отмена'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            _deleteTasks();
+            Navigator.of(context, rootNavigator: true).pop();
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.white,
+          ),
+          child: const Text('Удалить'),
+        ),
+      ],
+    );
+  }
+
+  Future _deleteTasks() async {
+    var collection = FirebaseFirestore.instance.collection('tasks');
+    collection.doc(widget.taskId).delete();
+  }
+}
